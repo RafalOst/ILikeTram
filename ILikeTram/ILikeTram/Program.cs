9af62d4ce85c2accf0792fe2b757449ae5c6e4ed
Rafal Ostachowicz <rafal.ostachowicz@gmail.com>
2021-10-12 13:47:29 +0200

    Add project

---
+++ ILikeTram/ILikeTram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ILikeTram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
