aebe3d837f29bb6c8c0147d0e8497f63b3386bf6
Rafal Ostachowicz <rafal.ostachowicz@gmail.com>
2021-10-12 14:35:18 +0200

    Add migration

---
+++ ILikeTram/ILikeTram/Migrations/20211012122516_first.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ILikeTram.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TramRoutes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TramRoutes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trams",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    FirstRun = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trams", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TramStops",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    RouteId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TramStops", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_TramStops_TramRoutes_RouteId",
+                        column: x => x.RouteId,
+                        principalTable: "TramRoutes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TramStops_RouteId",
+                table: "TramStops",
+                column: "RouteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trams");
+
+            migrationBuilder.DropTable(
+                name: "TramStops");
+
+            migrationBuilder.DropTable(
+                name: "TramRoutes");
+        }
+    }
+}
