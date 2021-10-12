7c5add31316180914128fa4e5e94421e71d67160
Rafal Ostachowicz <rafal.ostachowicz@gmail.com>
2021-10-12 14:24:20 +0200

    add TramDao

---
+++ ILikeTram/ILikeTram/DAL/DAO/TramDao.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ILikeTram.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ILikeTram.DAL.DAO
+{
+    public class TramDao : IDao<Tram>
+    {
+        private DatabaseContext Context { get; set; }
+
+        public TramDao(DatabaseContext context)
+        {
+            Context = context;
+        }
+
+        public void Add(Tram instance)
+        {
+            Context.Trams.Add(instance);
+            Context.SaveChanges();
+        }
+
+        public void Delete(Tram instance)
+        {
+            Context.Trams.Remove(instance);
+            Context.SaveChanges();
+        }
+
+        public Tram Read(string name)
+        {
+            return Context.Trams.AsNoTracking().Where(x => x.Name == name).FirstOrDefault();
+        }
+
+        public List<Tram> ReadAll()
+        {
+            return Context.Trams.AsNoTracking().ToList();
+        }
+
+        public void Update(Tram instance)
+        {
+            Context.Trams.Update(instance);
+            Context.SaveChanges();
+        }
+    }
+}
