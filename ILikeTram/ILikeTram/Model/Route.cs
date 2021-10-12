e47cea2dce4d0708e29143ce14e9d58c828d9d23
Rafal Ostachowicz <rafal.ostachowicz@gmail.com>
2021-10-12 14:03:47 +0200

    Add models with logic - Route - Tram - TramStop

---
+++ ILikeTram/ILikeTram/Model/Route.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ILikeTram.Model
+{
+    public class Route
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int? Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<TramStop> StopsList { get; set; }
+    }
+}
---
+++ ILikeTram/ILikeTram/Model/Tram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ILikeTram.Model
+{
+    public class Tram
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int? ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime FirstRun { get; set; }
+
+    }
+}
